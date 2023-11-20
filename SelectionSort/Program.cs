c6dc462245f0efcdbf8f791a944cc761085e6e72
Furkan ÇAM <120260143+Furkanncm@users.noreply.github.com>
2023-11-20 15:42:54 +0300

    selectionSort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 8, 7, 14, 2, 10 };
+            for(int i = 0; i<arr.Length-1;i++)
+            {
+                int min = i;
+
+                for (int j = i+1; j<arr.Length; j++)
+                {
+                    if (arr[j]<arr[min])
+                    {
+                        min=j;
+                    }
+                }
+
+                int temp = arr[min];
+                arr[min]=arr[i];
+                arr[i]=temp;
+               
+
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+          
+        }
+    }
+}
